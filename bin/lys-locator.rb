00a0697f5d4328a4736959c446744a4b72d0bd3b
lpassamano <leighayna.josele@gmail.com>
2017-07-20 21:03:13 -0400

    Adding config and bin folders

---
+++ bin/lys-locator.rb
@@ -0,0 +1,13 @@
+require_relative "../config/environment"
+
+
+#get location in form of zip code or city/state/country
+
+#return a list of lys near the location organized by rating
+
+#user selects a store to get more information about
+
+#asks user if they want to return to list or search again
+  #if return then go back to list of lys near location organized by rating
+  #elseif search then start program again
+  #else exit and give a goodbye message
