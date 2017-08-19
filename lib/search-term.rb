3527648d445d498e0f7fabd941cb26f70240d496
lpassamano <leighayna.josele@gmail.com>
2017-08-19 16:09:14 -0400

    added SearchTerm#initialize

--- lib/search-term.rb
+++ lib/search-term.rb
@@ -1,6 +1,10 @@
 class SearchTerm
   # stores all Store instances in an array  that are a result of a certain search term
-  attr_accessor :search_term
-  stores = []
+  attr_accessor :search_term, :stores
+
+  def initialize (search_term)
+    @search_term = search_term
+    @stores = []
+  end
 
 end
