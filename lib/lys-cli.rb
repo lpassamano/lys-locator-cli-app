6e898c55293be88c012371c5a9a8e05fa49a2054
lpassamano <leighayna.josele@gmail.com>
2017-08-21 18:11:10 -0400

    moved CLI notes to notes file

--- lib/lys-cli.rb
+++ lib/lys-cli.rb
@@ -45,38 +45,3 @@ class CLI_Interface
   end
 
 end
-
-#if no results and they did not input a zipcode
-    #ask to submit a zipcode to get more accurate results
-
-#user selects a store to get more information about
-
-#asks user if they want to return to list or search again
-  #if return then go back to list of lys near location organized by rating
-  #elseif search then start program again
-  #else exit and give a goodbye message
-
-
-#### CLI INTERFACE LAYOUT ####
-#
-# Welcome to LYS Locator!
-# Type in any zip code to find a yarn store near you:
-
-# => 08901
-
-# Your local yarn stores are:
-# 1. Store, rating
-# 2. Store, rating
-# Enter the number of the store to get more information.
-
-# => 1
-
-# Store, rating
-# Address
-# City, State, zip
-# hours
-# Website
-#
-# Would you like to return to the list, search again, or exit?
-
-# =>exit
