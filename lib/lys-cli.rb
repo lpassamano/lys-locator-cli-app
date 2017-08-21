d0fe7e1d9165084e5d381249d29544d34dd0591d
lpassamano <leighayna.josele@gmail.com>
2017-08-21 18:19:44 -0400

    created #more_information

--- lib/lys-cli.rb
+++ lib/lys-cli.rb
@@ -10,12 +10,37 @@ class CLI_Interface
     puts "Type in any zip code to find a yarn store near you:"
     location = gets.strip.delete(" ")
     #add in step to remove any spaces from international zip codes
+
+    #should new SearchTerm be created here or in Scraper
     SearchTerm.new(location)
 
     self.stores = Scraper.search(location)
     self.stores.each.with_index do |store, i|
       puts "#{i + 1}. #{store.name}, #{store.rating} stars"
     end
+    
+    more_information
+
+    puts "Would you like to return to the list, search, or exit?"
+    user_choice = gets.strip.downcase
+    case user_choice
+    when "list"
+      #access array of stores from the SearchTerm or Scraper class instead?
+      self.stores.each.with_index do |store, i|
+        puts "#{i + 1}. #{store.name}, #{store.rating} stars"
+      end
+      more_information
+    when "search"
+      #fix this later after #run is split into multiple methods
+      self.run
+    when "exit"
+      puts "Thanks for using LYS Locator!"
+    else
+      puts "Please select 'list' to return to your search, 'search' to search again, or 'exit' to leave LYS Locator."
+    end
+  end
+
+  def more_information
     puts "Type the number of the store to get more information:"
     more_info_index = gets.strip.to_i - 1
     store_info = self.stores[more_info_index]
@@ -27,21 +52,6 @@ class CLI_Interface
     puts "#{store_info.phone_number}"
     puts "#{store_info.website}"
     puts "#{store_info.hours}"
-
-    puts "Would you like to return to the list, search, or exit?"
-    user_choice = gets.strip.downcase
-    case user_choice
-    when "list"
-      #access array of stores from the SearchTerm or Scraper class?
-      # or just rerun the scraper
-    when "search"
-      #fix this later after #run is split into multiple methods
-      self.run
-    when "exit"
-      puts "Thanks for using LYS Locator!"
-    else
-      puts "Please select 'list' to return to your search, 'search' to search again, or 'exit' to leave LYS Locator."
-    end
   end
 
 end
